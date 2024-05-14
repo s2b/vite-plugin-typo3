f3bb200f53ba7099b8f9a90e132228b07b50b943
Simon Praetorius <moin@praetorius.me>
2024-05-14 18:40:37 +0200

    [BUGFIX] Allow empty plugin config

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import autoOrigin from "vite-plugin-auto-origin";
 import typo3composer from "./typo3composer.js";
 import type { VitePluginTypo3Config } from "./types.js";
 
-export default function typo3(userConfig: VitePluginTypo3Config) {
+export default function typo3(userConfig: VitePluginTypo3Config = {}) {
     return [typo3composer(userConfig), autoOrigin()];
 }
