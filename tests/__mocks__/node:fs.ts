9a663c1dc06d3c888a98de80f73d186426870bf9
Simon Praetorius <moin@praetorius.me>
2024-05-23 14:43:03 +0200

    [TASK] Switch to node 20 in CI

--- tests/__mocks__/node:fs.ts
+++ tests/__mocks__/node:fs.ts
@@ -7,18 +7,28 @@ const fs = jest.requireActual<typeof import("node:fs")>(`node:fs`);
 const fixturePath = resolve(__dirname, "../fixtures");
 const fixtureMount = "/path/to/fixtures";
 
-function createFixtureDirectoryStructure(path: string, replaceRoot: string, root = path): DirectoryJSON {
+function createFixtureDirectoryStructure(
+    path: string,
+    replaceRoot: string,
+    root = path,
+): DirectoryJSON {
     let files: any = {};
     const items = fs.readdirSync(path);
     for (let item of items) {
         item = join(path, item);
         const stat = fs.lstatSync(item);
         if (stat.isDirectory()) {
-            files = { ...files, ...createFixtureDirectoryStructure(item, replaceRoot, root) };
+            files = {
+                ...files,
+                ...createFixtureDirectoryStructure(item, replaceRoot, root),
+            };
         } else if (stat.isFile()) {
-            files[item.replace(root, replaceRoot)] = fs.readFileSync(item, 'utf8');
+            files[item.replace(root, replaceRoot)] = fs.readFileSync(
+                item,
+                "utf8",
+            );
         }
-    };
+    }
     return files;
 }
 
