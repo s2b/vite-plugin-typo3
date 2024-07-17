64a066c9a940a0e0c95120789e553bdcdae8299f
Simon Praetorius <moin@praetorius.me>
2024-07-17 20:08:25 +0200

    [TASK] Build real package for distribution test

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import autoOrigin from "vite-plugin-auto-origin";
 import type { UserConfig } from "./types.js";
 import typo3project from "./typo3project.js";
 import typo3extension from "./typo3extension.js";
+import { PluginOption } from "vite";
 
-export default function typo3(userConfig: UserConfig = {}) {
+export default function typo3(userConfig: UserConfig = {}): PluginOption[] {
     if (userConfig.target === "extension") {
         return [typo3extension(userConfig)];
     } else {
